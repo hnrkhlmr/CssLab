4acdafcde183a4c4970da3f7e01e5ec8e8a8eee7
Henrik Holmer <henrik.holmer@softronic.se>
2022-12-17 01:19:44 +0100

    lagt till nav med bootstrap

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -11,6 +13,8 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
 
